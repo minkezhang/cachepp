9f8f2c1d5e6a927a066dd6543c24e949eb8d3391
Minke Zhang <minke.zhang@gmail.com>
2014-08-09 00:08:55 -0500

    added more scaffolding to line.h

--- include/src/line.h
+++ include/src/line.h
@@ -1,6 +1,9 @@
 #ifndef _CACHEPP_LINE_H
 #define _CACHEPP_LINE_H
 
+#include <memory>
+#include <vector>
+
 #include "src/globals.h"
 
 /**
@@ -8,13 +11,31 @@
  */
 
 namespace cachepp {
+	/**
+	 * base cache line interface
+	 */
 	class Line {
 		public:
-			Line();
+			Line(identifier id);
 
-			virtual cachepp::identifier get_identifier() = 0;
+			identifier get_identifier();
+
+			/**
+			 * loads and unloads data from RAM
+			 */
 			virtual void load() = 0;
 			virtual void unload() = 0;
+
+		protected:
+			identifier id;
+			std::shared_ptr<std::vector<uint8_t>> data;
+
+			/**
+			 * checks the data in this->data
+			 *
+			 * raises exceptionpp::RuntimeError if the data is not valid
+			 */
+			virtual void checksum() = 0;
 	};
 }
 
--- src/line.cc
+++ src/line.cc
@@ -1,1 +1,5 @@
 #include "src/line.h"
+
+cachepp::Line::Line(cachepp::identifier id) : id(id) {}
+
+cachepp::identifier cachepp::Line::get_identifier() { return(this->id); }
