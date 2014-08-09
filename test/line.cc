dfb3bde610188b6a368bada88326233fbad1df2a
Minke Zhang <minke.zhang@gmail.com>
2014-08-09 02:24:48 -0500

    memory tests passing

--- test/line.cc
+++ test/line.cc
@@ -4,27 +4,27 @@
 #include "src/line.h"
 
 TEST_CASE("cachepp|line") {
-	cachepp::SimpleLine s = cachepp::SimpleLine(111, false);
+	std::shared_ptr<cachepp::SimpleLine> s (new cachepp::SimpleLine(111, false));
 
-	REQUIRE(s.get_is_loaded() == false);
-	REQUIRE(s.get_is_dirty() == false);
-	REQUIRE_THROWS_AS(s.get_data(), exceptionpp::RuntimeError);
-	s.load();
-	REQUIRE(s.get_is_loaded() == true);
-	REQUIRE(s.get_data()->at(0) == 111);
-	s.load();
-	REQUIRE(s.get_is_loaded() == true);
-	s.unload();
-	REQUIRE(s.get_is_loaded() == false);
-	REQUIRE(s.get_is_dirty() == true);
-	s.unload();
-	REQUIRE(s.get_is_loaded() == false);
-	s.load();
-	REQUIRE(s.get_is_loaded() == true);
+	REQUIRE(s->get_is_loaded() == false);
+	REQUIRE(s->get_is_dirty() == false);
+	REQUIRE_THROWS_AS(s->get_data(), exceptionpp::RuntimeError);
+	s->load();
+	REQUIRE(s->get_is_loaded() == true);
+	REQUIRE(s->get_data()->at(0) == 111);
+	s->load();
+	REQUIRE(s->get_is_loaded() == true);
+	s->unload();
+	REQUIRE(s->get_is_loaded() == false);
+	REQUIRE(s->get_is_dirty() == true);
+	s->unload();
+	REQUIRE(s->get_is_loaded() == false);
+	s->load();
+	REQUIRE(s->get_is_loaded() == true);
 
-	cachepp::SimpleLine t = cachepp::SimpleLine(112, true);
-	t.load();
-	t.unload();
-	REQUIRE(t.get_is_dirty() == true);
-	REQUIRE_THROWS_AS(t.load(), exceptionpp::RuntimeError);
+	std::shared_ptr<cachepp::SimpleLine> t (new cachepp::SimpleLine(111, true));
+	t->load();
+	t->unload();
+	REQUIRE(t->get_is_dirty() == true);
+	REQUIRE_THROWS_AS(t->load(), exceptionpp::RuntimeError);
 }
