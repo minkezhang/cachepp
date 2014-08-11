4b3b869c367d56e529df9d3ebd0ea555a6fd4787
Minke Zhang <minke.zhang@gmail.com>
2014-08-11 01:01:47 -0500

    added concurrent cache tests

--- test/cache.cc
+++ test/cache.cc
@@ -11,6 +11,56 @@
 
 TEST_CASE("cachepp|concurrentcache") {
 	std::shared_ptr<cachepp::SimpleConcurrentCache<cachepp::SimpleLine>> c (new cachepp::SimpleConcurrentCache<cachepp::SimpleLine>(2));
+
+	std::vector<std::shared_ptr<cachepp::SimpleLine>> v;
+	for(size_t i = 0; i < 10; ++i) {
+		v.push_back(std::shared_ptr<cachepp::SimpleLine> (new cachepp::SimpleLine(rand(), false)));
+	}
+
+	for(size_t i = 0; i < 10; ++i) {
+		REQUIRE(v.at(i)->get_is_loaded() == false);
+	}
+
+	// test cache line juggling
+	for(size_t i = 0; i < 10; ++i) {
+		c->acquire(v.at(i));
+		REQUIRE(v.at(i)->get_is_loaded() == true);
+	}
+
+	size_t loaded_lines = 0;
+	for(size_t i = 0; i < 10; ++i) {
+		loaded_lines += v.at(i)->get_is_loaded();
+	}
+
+	REQUIRE(loaded_lines == 2);
+
+	c->clear();
+
+	loaded_lines = 0;
+	for(size_t i = 0; i < 10; ++i) {
+		loaded_lines += v.at(i)->get_is_loaded();
+	}
+
+	REQUIRE(loaded_lines == 0);
+
+	// test selection policy
+	c->acquire(v.at(0));
+	c->acquire(v.at(1));
+
+	REQUIRE_THROWS_AS(c->access(v.at(2)), exceptionpp::RuntimeError);
+
+	c->access(v.at(0));
+	c->acquire(v.at(2));
+
+	REQUIRE(v.at(0)->get_is_loaded() == true);
+	REQUIRE(v.at(1)->get_is_loaded() == false);
+	REQUIRE(v.at(2)->get_is_loaded() == true);
+
+	c->acquire(v.at(3));
+
+	REQUIRE(v.at(0)->get_is_loaded() == false);
+	REQUIRE(v.at(2)->get_is_loaded() == true);
+	REQUIRE(v.at(3)->get_is_loaded() == true);
 }
 
 TEST_CASE("cachepp|serialcache") {
