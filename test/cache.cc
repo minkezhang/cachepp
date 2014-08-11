63116c87524c1c6a6fbc9f13c10642e6022742ce
Minke Zhang <minke.zhang@gmail.com>
2014-08-11 12:57:48 -0500

    tests now passing

--- test/cache.cc
+++ test/cache.cc
@@ -3,14 +3,136 @@
 #include <memory>
 #include <vector>
 
+#include <iostream>
+
 #include "libs/catch/catch.hpp"
 #include "libs/exceptionpp/exception.h"
 
 #include "src/simpleconcurrentcache.h"
 #include "src/simpleserialcache.h"
 #include "src/simpleline.h"
-void concurrentcache_multithread_worker(std::shared_ptr<std::atomic<size_t>> result, std::shared_ptr<cachepp::SimpleConcurrentCache<cachepp::SimpleLine>> c, std::vector<std::shared_ptr<cachepp::SimpleLine>> v) {
+
+void concurrentcache_multithread_worker(std::shared_ptr<std::atomic<size_t>> result, std::shared_ptr<cachepp::SimpleConcurrentCache<cachepp::SimpleLine>> c, std::shared_ptr<std::vector<std::shared_ptr<cachepp::SimpleLine>>> v) {
 	*result += 1;
+}
+
+
+TEST_CASE("cachepp|serialcache") {
+	std::shared_ptr<cachepp::SimpleSerialCache<cachepp::SimpleLine>> c (new cachepp::SimpleSerialCache<cachepp::SimpleLine>(2));
+
+	std::vector<std::shared_ptr<cachepp::SimpleLine>> v (0);
+	for(size_t i = 0; i < 10; ++i) {
+		v.push_back(std::shared_ptr<cachepp::SimpleLine> (new cachepp::SimpleLine(i, false)));
+	}
+
+	for(size_t i = 0; i < 10; ++i) {
+		REQUIRE(v.at(i)->get_is_loaded() == false);
+	}
+
+	/**
+	 * test cache line juggling
+	 */
+
+	for(size_t i = 0; i < 10; ++i) {
+		c->acquire(v.at(i));
+		REQUIRE(v.at(i)->get_is_loaded() == true);
+	}
+
+	size_t loaded_lines = 0;
+	for(size_t i = 0; i < 10; ++i) {
+		loaded_lines += v.at(i)->get_is_loaded();
+	}
+
+	REQUIRE(loaded_lines == 2);
+
+	c->clear();
+
+	loaded_lines = 0;
+	for(size_t i = 0; i < 10; ++i) {
+		loaded_lines += v.at(i)->get_is_loaded();
+	}
+
+	REQUIRE(loaded_lines == 0);
+
+	/**
+	 * test selection policy
+	 */
+
+	c->acquire(v.at(0));
+	c->acquire(v.at(1));
+
+	REQUIRE_THROWS_AS(c->access(v.at(2)), exceptionpp::RuntimeError);
+
+	c->access(v.at(0));
+	c->acquire(v.at(2));
+
+	REQUIRE(v.at(0)->get_is_loaded() == true);
+	REQUIRE(v.at(1)->get_is_loaded() == false);
+	REQUIRE(v.at(2)->get_is_loaded() == true);
+
+	c->acquire(v.at(3));
+
+	REQUIRE(v.at(0)->get_is_loaded() == true);
+	REQUIRE(v.at(3)->get_is_loaded() == true);
+}
+
+TEST_CASE("cachepp|concurrentcache-singlethread") {
+	std::shared_ptr<cachepp::SimpleConcurrentCache<cachepp::SimpleLine>> c (new cachepp::SimpleConcurrentCache<cachepp::SimpleLine>(2));
+
+	std::vector<std::shared_ptr<cachepp::SimpleLine>> v (0);
+	for(size_t i = 0; i < 10; ++i) {
+		v.push_back(std::shared_ptr<cachepp::SimpleLine> (new cachepp::SimpleLine(i, false)));
+	}
+
+	for(size_t i = 0; i < 10; ++i) {
+		REQUIRE(v.at(i)->get_is_loaded() == false);
+	}
+
+	/**
+	 * test cache line juggling
+	 */
+
+	for(size_t i = 0; i < 10; ++i) {
+		c->acquire(v.at(i));
+		REQUIRE(v.at(i)->get_is_loaded() == true);
+	}
+
+	size_t loaded_lines = 0;
+	for(size_t i = 0; i < 10; ++i) {
+		loaded_lines += v.at(i)->get_is_loaded();
+	}
+
+	REQUIRE(loaded_lines == 2);
+
+	c->clear();
+
+	loaded_lines = 0;
+	for(size_t i = 0; i < 10; ++i) {
+		loaded_lines += v.at(i)->get_is_loaded();
+	}
+
+	REQUIRE(loaded_lines == 0);
+
+	/**
+	 * test selection policy
+	 */
+
+	c->acquire(v.at(0));
+	c->acquire(v.at(1));
+
+	REQUIRE_THROWS_AS(c->access(v.at(2)), exceptionpp::RuntimeError);
+
+	c->access(v.at(0));
+	c->acquire(v.at(2));
+
+	REQUIRE(v.at(0)->get_is_loaded() == true);
+	REQUIRE(v.at(1)->get_is_loaded() == false);
+	REQUIRE(v.at(2)->get_is_loaded() == true);
+
+	c->acquire(v.at(3));
+
+	REQUIRE(v.at(0)->get_is_loaded() == true);
+	REQUIRE(v.at(3)->get_is_loaded() == true);
 }
 
 TEST_CASE("cachepp|concurrentcache-multithread") {
@@ -18,10 +140,10 @@ TEST_CASE("cachepp|concurrentcache-multithread") {
 	size_t n_attempts = 1000;
 
 	std::shared_ptr<cachepp::SimpleConcurrentCache<cachepp::SimpleLine>> c (new cachepp::SimpleConcurrentCache<cachepp::SimpleLine>(2));
+	std::shared_ptr<std::vector<std::shared_ptr<cachepp::SimpleLine>>> v (new std::vector<std::shared_ptr<cachepp::SimpleLine>> (0));
 
-	std::vector<std::shared_ptr<cachepp::SimpleLine>> v (0);
 	for(size_t i = 0; i < 10; ++i) {
-		v.push_back(std::shared_ptr<cachepp::SimpleLine> (new cachepp::SimpleLine(rand(), false)));
+		v->push_back(std::shared_ptr<cachepp::SimpleLine> (new cachepp::SimpleLine(i, false)));
 	}
 
 	std::shared_ptr<std::atomic<size_t>> result (new std::atomic<size_t>());
@@ -31,124 +153,4 @@ TEST_CASE("cachepp|concurrentcache-multithread") {
 		}
 	}
 	REQUIRE(*result == (n_attempts * n_threads));
-}
-
-TEST_CASE("cachepp|concurrentcache-singlethread") {
-	std::shared_ptr<cachepp::SimpleConcurrentCache<cachepp::SimpleLine>> c (new cachepp::SimpleConcurrentCache<cachepp::SimpleLine>(2));
-
-	std::vector<std::shared_ptr<cachepp::SimpleLine>> v (0);
-	for(size_t i = 0; i < 10; ++i) {
-		v.push_back(std::shared_ptr<cachepp::SimpleLine> (new cachepp::SimpleLine(rand(), false)));
-	}
-
-	for(size_t i = 0; i < 10; ++i) {
-		REQUIRE(v.at(i)->get_is_loaded() == false);
-	}
-
-	/**
-	 * test cache line juggling
-	 */
-
-	for(size_t i = 0; i < 10; ++i) {
-		c->acquire(v.at(i));
-		REQUIRE(v.at(i)->get_is_loaded() == true);
-	}
-
-	size_t loaded_lines = 0;
-	for(size_t i = 0; i < 10; ++i) {
-		loaded_lines += v.at(i)->get_is_loaded();
-	}
-
-	REQUIRE(loaded_lines == 2);
-
-	c->clear();
-
-	loaded_lines = 0;
-	for(size_t i = 0; i < 10; ++i) {
-		loaded_lines += v.at(i)->get_is_loaded();
-	}
-
-	REQUIRE(loaded_lines == 0);
-
-	/**
-	 * test selection policy
-	 */
-
-	c->acquire(v.at(0));
-	c->acquire(v.at(1));
-
-	REQUIRE_THROWS_AS(c->access(v.at(2)), exceptionpp::RuntimeError);
-
-	c->access(v.at(0));
-	c->acquire(v.at(2));
-
-	REQUIRE(v.at(0)->get_is_loaded() == true);
-	REQUIRE(v.at(1)->get_is_loaded() == false);
-	REQUIRE(v.at(2)->get_is_loaded() == true);
-
-	c->acquire(v.at(3));
-
-	REQUIRE(v.at(0)->get_is_loaded() == false);
-	REQUIRE(v.at(2)->get_is_loaded() == true);
-	REQUIRE(v.at(3)->get_is_loaded() == true);
-}
-
-TEST_CASE("cachepp|serialcache") {
-	std::shared_ptr<cachepp::SimpleSerialCache<cachepp::SimpleLine>> c (new cachepp::SimpleSerialCache<cachepp::SimpleLine>(2));
-
-	std::vector<std::shared_ptr<cachepp::SimpleLine>> v (0);
-	for(size_t i = 0; i < 10; ++i) {
-		v.push_back(std::shared_ptr<cachepp::SimpleLine> (new cachepp::SimpleLine(rand(), false)));
-	}
-
-	for(size_t i = 0; i < 10; ++i) {
-		REQUIRE(v.at(i)->get_is_loaded() == false);
-	}
-
-	/**
-	 * test cache line juggling
-	 */
-
-	for(size_t i = 0; i < 10; ++i) {
-		c->acquire(v.at(i));
-		REQUIRE(v.at(i)->get_is_loaded() == true);
-	}
-
-	size_t loaded_lines = 0;
-	for(size_t i = 0; i < 10; ++i) {
-		loaded_lines += v.at(i)->get_is_loaded();
-	}
-
-	REQUIRE(loaded_lines == 2);
-
-	c->clear();
-
-	loaded_lines = 0;
-	for(size_t i = 0; i < 10; ++i) {
-		loaded_lines += v.at(i)->get_is_loaded();
-	}
-
-	REQUIRE(loaded_lines == 0);
-
-	/**
-	 * test selection policy
-	 */
-
-	c->acquire(v.at(0));
-	c->acquire(v.at(1));
-
-	REQUIRE_THROWS_AS(c->access(v.at(2)), exceptionpp::RuntimeError);
-
-	c->access(v.at(0));
-	c->acquire(v.at(2));
-
-	REQUIRE(v.at(0)->get_is_loaded() == true);
-	REQUIRE(v.at(1)->get_is_loaded() == false);
-	REQUIRE(v.at(2)->get_is_loaded() == true);
-
-	c->acquire(v.at(3));
-
-	REQUIRE(v.at(0)->get_is_loaded() == false);
-	REQUIRE(v.at(2)->get_is_loaded() == true);
-	REQUIRE(v.at(3)->get_is_loaded() == true);
 }
