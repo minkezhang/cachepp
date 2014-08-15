906e3bf064585cd9352c1b5f1a8f630b05608b91
Minke Zhang <minke.zhang@gmail.com>
2014-08-15 18:06:13 -0500

    reverted main.cc changes

--- tutorial/src/main.cc
+++ tutorial/src/main.cc
@@ -1,12 +1,8 @@
 #include <iostream>
 
-#include "libs/cachepp/simpleline.h"
-#include "src/lru.h"
-
 int main() {
 	std::cout << "This is a skeletal tutorial application for the cachepp caching framework." << std::endl << std::endl \
 		<< "Please see https://github.com/cripplet/cachepp/blob/master/docs/overview.md for the tutorial." << std::endl;
 
-	std::shared_ptr<LRUCache<cachepp::SimpleLine>> c (new LRUCache<cachepp::SimpleLine>(1));
 	return(0);
 }
